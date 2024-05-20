597ea07edbce2ff223fcb6e286e63d1bbdc022f4
NgAnhDUc <86831818+NgAnhDUc@users.noreply.github.com>
2024-05-20 17:33:25 +0700

    Dash Effect

--- Assets/MyScripts/Player/PlayerDash.cs
+++ Assets/MyScripts/Player/PlayerDash.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public class PlayerDash : MonoBehaviour
 {
-    
+    public GameObject dashEffectGO;
+    public Animator dashAnim;
+    private void Awake()
+    {
+        this.dashEffectGO = GameObject.Find("EffectDash");
+        this.dashAnim = this.dashEffectGO.GetComponent<Animator>();
+    }
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.LeftShift))
         {
             transform.Translate(Vector2.right * 5.0f);
+            this.SetDashAnim();
         }
     }
+    protected void SetDashAnim()
+    {
+        this.dashAnim.SetBool("isDash", true);
+        Invoke("SetFinishDashAnim", 0.06f);
+    }
+    protected void SetFinishDashAnim()
+    {
+        this.dashAnim.SetBool("isDash", false);
+    }
+
 }

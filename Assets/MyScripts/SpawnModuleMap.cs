714d0bcac51d8520ab7eac021d63cfb44a8251ad
QuocHung3 <86833306+QuocHung3@users.noreply.github.com>
2024-05-23 16:42:53 +0700

    spawn module map

---
+++ Assets/MyScripts/SpawnModuleMap.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnModuleMap : Spawner
+{
+    public bool isSpawn = false;
+
+    private void Reset()
+    {
+        this.prefab = Resources.Load<GameObject>("MapModules/Map Module");
+        this.parent = GameObject.Find("Map Modules").transform;
+    }
+
+    private void Awake()
+    {
+        this.Reset();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isSpawn) return;
+        if (collision.gameObject.tag == "Player")
+        {
+            this.posisionSpawn = new Vector3(transform.position.x + 100f, 0, 0);
+            this.SpawnPrefabs(prefab, parent, quatity, posisionSpawn);
+
+            isSpawn = true;
+        }
+    }
+}
